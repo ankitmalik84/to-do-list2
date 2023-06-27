8f45f9bcdb05573e9d641590291e99048344ece9
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-06-27 22:24:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# ToDo JS ✔
+
+### Check it out [here]!
+
+ <img src="https://forthebadge.com/images/badges/made-with-javascript.svg">
+
+## Overview 👀
+
+<img src="gitres/pic.png" >
+
+- A simple To-Do list web app to keep a track of things you gotta do
+- This web app makes no request to external/backend servers for storing data but instead stores data on local machines
+- Fully responsive site
+
+## WIP 🛠
+
+- Adding edit functionality
+- Adding gesture events
+- Mobile APP
+
+## How To Contribute🤝
+
+- Yes,it is open source you can clone and change code to your need.
+- Most of the code is self explanatory, anyhow
+- for any clarification on code or want to contribute contact
+- [Gmail] / [Twitter] /[LinkedIn]
+
+[Gmail]: mailto:ankitmalik844903@gmail.com
+[LinkedIn]: https://www.linkedin.com/in/ankitmalik84
+[Twitter]: https://twitter.com/ankitmalik
+[here]: https:/ToDo-JS/
---
+++ app.js
@@ -0,0 +1,76 @@
+const addRef = document.querySelector(".add");
+const listRef = document.querySelector(".todos");
+const searchRef = document.querySelector(".search input");
+const submitRef = document.querySelector(".submit");
+const clean = document.querySelector(".clean");
+var todosList = [];
+const generateTemplate = (todo) => {
+  const htmlTemplate = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="fas fa-times delete" style="color:red;"></i>
+    </li>`;
+  listRef.innerHTML = htmlTemplate + listRef.innerHTML;
+};
+
+const filterTodo = (term) => {
+  Array.from(listRef.children)
+    .filter(
+      (todo) => !todo.textContent.toLowerCase().includes(term.toLowerCase())
+    )
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(listRef.children)
+    .filter((todo) =>
+      todo.textContent.toLowerCase().includes(term.toLowerCase())
+    )
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+searchRef.addEventListener("keyup", () => {
+  const term = searchRef.value.trim();
+  filterTodo(term);
+});
+
+listRef.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+    const toDelete = e.target.parentElement.textContent.trim();
+    todosList = todosList.filter((item) => {
+      return item !== toDelete;
+    });
+    localStorage.clear();
+    localStorage.setItem("prevTodos", JSON.stringify(todosList));
+  }
+});
+
+addRef.addEventListener("submit", (e) => {
+  e.preventDefault();
+  add();
+});
+const add = () => {
+  const newTodo = addRef.inputField.value.trim();
+  todosList.push(newTodo);
+  localStorage.setItem("prevTodos", JSON.stringify(todosList));
+  if (newTodo !== "") {
+    generateTemplate(newTodo);
+  }
+  addRef.reset();
+};
+
+submitRef.addEventListener("click", (e) => {
+  add();
+});
+
+clean.addEventListener("click", () => {
+  listRef.innerHTML = "";
+  todosList = [];
+  localStorage.clear();
+  localStorage.setItem("todosList", JSON.stringify(todosList));
+});
+
+if (localStorage.getItem("prevTodos")) {
+  JSON.parse(localStorage.getItem("prevTodos")).forEach((Element) => {
+    todosList.push(Element);
+    generateTemplate(Element);
+  });
+}
